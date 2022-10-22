cf6e3c85d447990aece30eb8d88ae6db62701b8a
Your Name <you@example.com>
2022-10-23 00:49:13 +0100

    modified:   src/app.module.ts 	new file:   src/premier/premier.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PremierModule } from './premier/premier.module';
 
 @Module({
-  imports: [],
+  imports: [PremierModule],
   controllers: [AppController],
   providers: [AppService],
 })
