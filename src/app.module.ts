769a1bedfe3fd3cc2d06057ad079525f9fdbcb6b
Your Name <you@example.com>
2022-10-23 19:07:52 +0100

    modified:   src/app.module.ts 	new file:   src/pipe/pipe.controller.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { PremierModule } from './premier/premier.module';
 import { TodoModule } from './todo/todo.module';
 import { CommonModule } from './common/common.module';
+import {PipeController} from './pipe/pipe.controller';
 
 @Module({
   imports: [PremierModule, TodoModule, CommonModule],
-  controllers: [AppController],
+  controllers: [AppController, PipeController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/pipe/pipe.controller.ts
@@ -0,0 +1,8 @@
+import { Controller, Injectable } from "@nestjs/common";
+
+@Controller('pipes')
+@Injectable()
+
+export class PipeController{
+
+}
